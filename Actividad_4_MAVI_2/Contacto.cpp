d3f9e4f8a76f4865bab9e95cddf89158b51f3dd8
Baltazarsacchi <baltazarsacchi269@gmail.com>
2026-06-28 19:56:37 -0300

    Update Contacto.cpp

--- Actividad_4_MAVI_2/Contacto.cpp
+++ Actividad_4_MAVI_2/Contacto.cpp
@@ -2,18 +2,18 @@
 #include "box2d.h"
 #include "Cuerpos.h"
 
-void Contacto::BeginContact(b2Contact* contacto) {
+void Contacto::BeginContact(b2Contact* contacto) {//Deteccion de cuando comienza el contacto
 
-	Cuerpos* a = (Cuerpos*)contacto->GetFixtureA()->GetBody()->GetUserData().pointer;
+	Cuerpos* a = (Cuerpos*)contacto->GetFixtureA()->GetBody()->GetUserData().pointer;//Almacena en un puntero de cuerpo a ambos objetos
 	Cuerpos* b = (Cuerpos*)contacto->GetFixtureB()->GetBody()->GetUserData().pointer;
 
-	if (!a || !b) { return; }
+	if (!a || !b) { return; }//Verifico que si existan
 
-	Cuerpos* cuerpo_1 = reinterpret_cast<Cuerpos*>(a);
+	Cuerpos* cuerpo_1 = reinterpret_cast<Cuerpos*>(a);//Hago que lo reinterprete a ambos valores para corroborar que no haya una falla
 	Cuerpos* cuerpo_2 = reinterpret_cast<Cuerpos*>(b);
 
 	if (!cuerpo_1 || !cuerpo_2) return;
 
-	cuerpo_1->colision(cuerpo_2);
+	cuerpo_1->colision(cuerpo_2);//Detecta si uno de los dos objetos colision con el otro
 	cuerpo_2->colision(cuerpo_1);
 }
