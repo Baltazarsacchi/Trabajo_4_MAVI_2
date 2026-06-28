b498855b618a1787d8ae5ff27482a5bee79d8306
Baltazarsacchi <baltazarsacchi269@gmail.com>
2026-06-28 19:59:16 -0300

    Update Enemigo.h

--- Actividad_4_MAVI_2/Enemigo.h
+++ Actividad_4_MAVI_2/Enemigo.h
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-const float Diferencia = 100.0f;
+const float Diferencia = 100.0f;//Escala que utilizo con la referencia de 1 metro = 100 px.
 
 class Enemigo : public Cuerpos {
 
@@ -26,20 +26,21 @@ private:
 
 public:
 
-	Enemigo(b2World& mundo, b2Vec2 pos, b2Vec2 tam, Texture2D tex, Texture2D tex_2);
+	Enemigo(b2World& mundo, b2Vec2 pos, b2Vec2 tam, Texture2D tex, Texture2D tex_2);//Cnstructor
 
+//Herencia de clase Cuerpos
 	void actualizacion() override;
 	void dibujar() override;
 
-	Proyectiles* disparo(b2World& mundo);
+	Proyectiles* disparo(b2World& mundo);//Crea su propio proyectil
 
-	void conexion(b2World& mundo, b2Body* cuerpo_2);
+	void conexion(b2World& mundo, b2Body* cuerpo_2);//Hace la conexion con un objeto para moverse
 
-	void colision(Cuerpos* otro) override;
+	void colision(Cuerpos* otro) override;//Detecta sus colisiones
 
-	bool vivo();
+	bool vivo();//Pregunta si esta vivo el enemigo
 
-	void reinicio();
+	void reinicio();//Reinicia su atributo de vida
 
 
 };
