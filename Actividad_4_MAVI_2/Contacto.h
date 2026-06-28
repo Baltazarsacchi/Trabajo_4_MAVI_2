3a8ee2fa6ad874fdd19cf7dd906639682d97cc38
Baltazarsacchi <baltazarsacchi269@gmail.com>
2026-06-28 19:49:47 -0300

    Update Contacto.h

--- Actividad_4_MAVI_2/Contacto.h
+++ Actividad_4_MAVI_2/Contacto.h
@@ -2,7 +2,7 @@
 #define CONTACTO_H
 #include "box2d.h"
 
-class Contacto : public b2ContactListener {
+class Contacto : public b2ContactListener {//clase que sirve para la deteccion de contactos.
 public:
     void BeginContact(b2Contact* contacto) override;
 };
