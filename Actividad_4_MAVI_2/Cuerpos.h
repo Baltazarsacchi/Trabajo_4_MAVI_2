c607a26c2500e06046cf83d99e85067049e671ec
Baltazarsacchi <baltazarsacchi269@gmail.com>
2026-06-28 19:51:20 -0300

    Update Cuerpos.h

--- Actividad_4_MAVI_2/Cuerpos.h
+++ Actividad_4_MAVI_2/Cuerpos.h
@@ -2,18 +2,18 @@
 #define CUERPOS_H
 #include "box2d.h"
 
-enum Tipos {
+enum Tipos {//Tipo de cuerpo
 
 	Proyectil,Personaje,Enemigos,Estructura
 
 
 };
-enum Dueno{
+enum Dueno{//De quien sale el proyectil
 
 	Nd,Pj,Em
 };
 
-class Cuerpos {
+class Cuerpos { //Clase base para la creacion de otros cuerpo y simplificacion de chequo de colisiones
 
 public:
 
